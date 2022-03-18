6b611ab4d58475241cb75bf6955f98ec0a48b1e6
jae <goawmfhfl1@naver.com>
2022-03-18 11:22:44 +0900

    Add: EditorList 생성 및 분기처리
    index값을 설정해면 안좋은 이유:
    CRUD를 통해서 index의 값들이 변경 될 경우 index값이 변경되므로 좋지 않다.
    
    하나하나 명시해서  보내지 않아도 괜찮구나.. 이번에 알았다.
    {diaryList.map((it) => (
        <DiaryItem key={it.id} {...it} />
    ))}
    
    전달받은 값은 아래와같이 입력이 가능하다.
    const DiaryItem = ({ author, content, created_date, emotion }) => {
      return <div>DiaryItem</div>;
    };
    
    현재 날짜 구해오는 방법
    1. 숫자를 불러온다
    created_date: new Date().getTime(),
    2. 다시 new Date의 인수로 넘긴 후 toLocalString 메서드를 사용한다.
    {new Date(created_date).toLocaleString()}
    
    현재 브랜치 Develop_DiaryList
    커밋할 변경 사항:
    	수정함:        src/App.js
    	새 파일:       src/DiaryItem.js
    	새 파일:       src/DiaryList.js

---
+++ src/DiaryItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const DiaryItem = ({ author, content, created_date, emotion }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자: {author} | 감성점수 : {emotion}{" "}
+        </span>
+        <br />
+        <span className="date"> {new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+export default DiaryItem;
