627f1b283d6a7f95d1c774be8b5c1e0496ff1c11
jae <goawmfhfl1@naver.com>
2022-03-18 15:10:22 +0900

    Refactoring: handleRemove 간소화하기 및 handleUpdate 추가

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -8,6 +8,16 @@ const DiaryItem = ({
   emotion,
   onRemove,
 }) => {
+  const handleRemove = () => {
+    console.log(id);
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleUpdate = () => {
+    console.log();
+  };
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -18,16 +28,8 @@ const DiaryItem = ({
         <span className="date"> {new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          console.log(id);
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onRemove(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <button onClick={handleRemove}>삭제하기</button>
+      <button onClick={handleUpdate}>수정하기</button>
     </div>
   );
 };
