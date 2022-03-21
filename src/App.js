17cf570cc5a78e71868cb0fa10d4405979a058ee
jae <goawmfhfl1@naver.com>
2022-03-21 21:26:08 +0900

    Add: 폰트 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
