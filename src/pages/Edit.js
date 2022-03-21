809f19483e56dcbf1d149c131f9fa79477dd71d7
jae <goawmfhfl1@naver.com>
2022-03-21 21:16:17 +0900

    Add: ReactRouting Test

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+  console.log(id);
+  console.log(mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <div>이곳은 일기 수정 페이지 입니다</div>
+      <button onClick={() => setSearchParams({ id: "1", mode: "white" })}>
+        setSearchParams
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        home button
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back button
+      </button>
     </div>
   );
 };
