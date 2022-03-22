6c22af361a185ef7bd7a2334be8b1d1744daaa70
jae <goawmfhfl1@naver.com>
2022-03-22 15:04:22 +0900

    Add: useNavigate를 통한 라우팅 적용

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Mybutton from "./Mybutton";
 
 const DiaryItem = ({ content, date, emotion, id }) => {
+  const navigate = useNavigate();
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
   const strDate = new Date(parseInt(date)).toLocaleDateString();
   return (
     <div className="DiaryItem">
@@ -10,18 +18,24 @@ const DiaryItem = ({ content, date, emotion, id }) => {
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
         ].join(" ")}
+        onClick={() => goDetail()}
       >
         <img
           src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}
           alt="감정 이미지"
         />
       </div>
-      <div className="info_wrapper">
+      <div className="info_wrapper" onClick={() => goDetail()}>
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
       <div className="btn_wrapper">
-        <Mybutton />
+        <Mybutton
+          text={"수정하기"}
+          onClick={() => {
+            goEdit();
+          }}
+        />
       </div>
     </div>
   );
