d3c453d729a1e57322e9d1cdaccac408dad08511
jae <goawmfhfl1@naver.com>
2022-03-22 11:09:26 +0900

    Add: 달력 년 월 일 나타내기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/Mybutton";
 
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
     <div>
-      <h1>Home</h1>
+      <MyHeader
+        headText={headText}
+        leftChild={
+          <MyButton
+            text={"<"}
+            onClick={() => {
+              decreaseMonth();
+            }}
+          />
+        }
+        rightChild={
+          <MyButton
+            text={">"}
+            onClick={() => {
+              increaseMonth();
+            }}
+          />
+        }
+      />
       <div>이곳은 홈 입니다</div>
     </div>
   );
