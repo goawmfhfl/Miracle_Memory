ddc5a8e170de05dd6a230bcb55f5c143d4ef5cd3
jae <goawmfhfl1@naver.com>
2022-03-24 14:03:23 +0900

    Add: 모듈작업 - Emotion 모듈 생성

---
+++ src/components/module/emotion/DetailEmotion.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const DetailEmotion = ({ emotion_descript, emotion, emotion_img }) => {
+  return (
+    <>
+      <EmotionImg className={emotion} src={emotion_img} />
+      <EmotionDescript>{emotion_descript}</EmotionDescript>
+    </>
+  );
+};
+
+const EmotionImg = styled.img.attrs({ alt: "이모션 이미지" })`
+  border-radius: 50%;
+
+  &.1 {
+    box-shadow: 0px 0px 20px #038d05;
+  }
+  &.2 {
+    box-shadow: 0px 0px 20px #5bb616;
+  }
+  &.3 {
+    box-shadow: 0px 0px 20px #cfa601;
+  }
+  &.4 {
+    box-shadow: 0px 0px 20px #f86d28;
+  }
+  &.5 {
+    box-shadow: 0px 0px 20px #bd131c;
+  }
+`;
+
+const EmotionDescript = styled.span`
+  font-size: 25px;
+  color: white;
+`;
+
+export default DetailEmotion;
