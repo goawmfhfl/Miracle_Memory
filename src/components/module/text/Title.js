94b81093778ceb4759f23ad2df30be42eb5a7d74
jae <goawmfhfl1@naver.com>
2022-03-24 15:02:04 +0900

    Add: Module 작업 - Module 부분 작업 완료

---
+++ src/components/module/text/Title.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = () => {
+  return <Text>Title</Text>;
+};
+
+const Text = styled.h1`
+  width: 50%;
+  font-size: 25px;
+  justify-content: center;
+`;
+export default Title;
