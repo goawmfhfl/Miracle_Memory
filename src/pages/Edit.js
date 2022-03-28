c3f02e16072b6f1a354a3d7ebdc7f80b550dae05
jae <goawmfhfl1@naver.com>
2022-03-28 19:47:48 +0900

    Add: Edit 페이지 데이터 바인딩

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import { DiaryStateContext } from "../context/DiaryContext";
+import { useNavigate, useParams } from "react-router-dom";
 import EditorContainer from "../components/template/EditorContainer";
-
 const Edit = () => {
-  return <EditorContainer />;
+  const { id } = useParams();
+  const [originData, setOriginData] = useState();
+  const navigate = useNavigate();
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (list) => parseInt(list.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+  return (
+    originData && <EditorContainer isEdit={true} originData={originData} />
+  );
 };
 export default Edit;
