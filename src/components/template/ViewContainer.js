15d874ef22f4673d87fbd5acd54eb098c304523b
jae <goawmfhfl1@naver.com>
2022-03-28 12:50:48 +0900

    Add: 날짜 데이터 불러오기 추상화

--- src/components/template/ViewContainer.js
+++ src/components/template/ViewContainer.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../../context/DiaryContext";
 import Button from "../molecule/etc/Button";
 import CommonHeader from "../organisms/common/CommonHeader";
 import MemoListItem from "../organisms/item/MemoListItem";
+import { getMonthDate } from "../../util/date";
 
 const ViewContainer = () => {
-  const increaseMonth = () => {};
-  const decreaseMonth = () => {};
+  const diaryList = useContext(DiaryStateContext);
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      getMonthDate.changeMonthDate(curDate, setData, diaryList);
+    }
+  }, [curDate, diaryList]);
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () => {
+    getMonthDate.increaseMonth(curDate, setCurDate);
+  };
+  const decreaseMonth = () => {
+    getMonthDate.decreaseMonth(curDate, setCurDate);
+  };
   return (
     <div>
       <CommonHeader
-        headText={"2022년 3월"}
+        headText={headText}
         leftChild={<Button text={"<"} onClick={decreaseMonth} />}
         rightChild={<Button text={">"} onClick={increaseMonth} />}
       />
-      <MemoListItem />
+      <MemoListItem diaryList={data} />
     </div>
   );
 };
--- src/util/date.js
+++ src/util/date.js
@@ -1,3 +1,35 @@
 export const getStringDate = (date) => {
   return date.toISOString().slice(0, 10);
 };
+
+export const getMonthDate = {
+  increaseMonth(curDate, setCurDate) {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1),
+      curDate.getDate()
+    );
+  },
+  decreaseMonth(curDate, setCurDate) {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1),
+      curDate.getDate()
+    );
+  },
+  changeMonthDate(curDate, setData, diaryList) {
+    const firstDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth(),
+      1
+    ).getTime();
+
+    const lastDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth() + 1,
+      0
+    ).getTime();
+
+    setData(
+      diaryList.filter((list) => firstDay <= list.date && list.date <= lastDay)
+    );
+  },
+};
