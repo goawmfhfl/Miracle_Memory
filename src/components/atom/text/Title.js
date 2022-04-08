43532c388793df9006639b88cf8e878bd43e15a9
jae <goawmfhfl1@naver.com>
2022-04-08 15:37:33 +0900

    Edit: Detail 페이지 타이틀 반응형

--- src/components/atom/text/Title.js
+++ src/components/atom/text/Title.js
@@ -8,5 +8,9 @@ const Text = styled.h1`
   font-size: 20px;
   justify-content: center;
   font-family: "MaruBuri-Bold";
+
+  @media (min-width: 390px) and (max-width: 600px) {
+    font-size: 17px;
+  }
 `;
 export default Title;
