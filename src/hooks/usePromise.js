3254af15ad760fb33a3334f04381dae4e9cfbac1
jae <goawmfhfl1@naver.com>
2022-04-15 19:04:18 +0900

    Edit: 변경사항 적용

--- src/hooks/usePromise.js
+++ src/hooks/usePromise.js
@@ -17,6 +17,7 @@ const usePromise = (promiseCreator, deps) => {
       setLoading(false);
     };
     process();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 
   return [loading, resolved, error];
