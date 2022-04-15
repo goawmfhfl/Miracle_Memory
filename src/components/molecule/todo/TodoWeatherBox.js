f93c0708ac7230cc3a15c2ccda97054406e91784
jae <goawmfhfl1@naver.com>
2022-04-15 18:59:40 +0900

    Edit: Request

--- src/components/molecule/todo/TodoWeatherBox.js
+++ src/components/molecule/todo/TodoWeatherBox.js
@@ -4,8 +4,6 @@ import axios from "axios";
 import CommonText from "../../atom/text/CommonText";
 import Icon from "../../atom/icon/Icon";
 import usePromise from "../../../hooks/usePromise";
-import dotenv from "dotenv";
-dotenv.config();
 
 const TodoWeatherBox = () => {
   const [loading, response, error] = usePromise(() => {
