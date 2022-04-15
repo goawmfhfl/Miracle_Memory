e965edbca90c29fcc241d9b0fd16ea936dd76ba5
jae <goawmfhfl1@naver.com>
2022-04-15 20:55:37 +0900

    Fix: API문제 해결

--- src/components/molecule/todo/TodoWeatherBox.js
+++ src/components/molecule/todo/TodoWeatherBox.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
-import firebaseConfig from "../../../firebaseconfig";
 import CommonText from "../../atom/text/CommonText";
 import Icon from "../../atom/icon/Icon";
 import usePromise from "../../../hooks/usePromise";
@@ -10,7 +9,7 @@ const TodoWeatherBox = () => {
   const [loading, response, error] = usePromise(() => {
     const location = JSON.parse(localStorage.getItem("location"));
     return axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${firebaseConfig.apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_WEATHER_KEY}&units=metric`
     );
   }, []);
 
--- src/firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_WEATHER_KEY,
-};
-
-const app = initializeApp(firebaseConfig);
-
-export default firebaseConfig;
